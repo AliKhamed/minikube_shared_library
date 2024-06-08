21b9439114694ae73d65c61e6c3d38f1b8954f7b
Ali khames <46407148+AliKhamed@users.noreply.github.com>
2024-06-09 00:50:30 +0300

    Update editNewImage.groovy

--- vars/editNewImage.groovy
+++ vars/editNewImage.groovy
@@ -7,13 +7,13 @@ def call(String githubToken, String imageName, String gitUserEmail, String gitUs
                     git config user.email "${gitUserEmail}"
                     git config user.name "${gitUserName}"
                     sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' k8s/deployment.yaml
-                    // sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" k8s/deployment.yaml
                     git add k8s/deployment.yaml
                     git commit -m "Update deployment image to version ${BUILD_NUMBER}"
                     git push https://${GITHUB_TOKEN}@github.com/${gitUserName}/${gitRepoName} HEAD:main
                 '''
     
     // // Edit deployment.yaml with new Docker Hub image
+    // sed -i "s/replaceImageTag/${BUILD_NUMBER}/g" k8s/deployment.yaml
     // sh "sed -i 's|image:.*|image: ${imageName}:${BUILD_NUMBER}|g' deployment.yaml"
 
 }
